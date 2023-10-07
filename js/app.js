2b2588ccb1d07db13e6986197cc84236756b42bc
elizabethpurtell <epurtell.CF23@gmail.com>
2023-10-07 16:20:17 -0700

    added js and updated html/css

--- js/app.js
+++ js/app.js
@@ -2,18 +2,19 @@
 
 
 // ***** GLOBALS ******
-let votingRounds = 15;
-let goatArray = [];
+let votingRounds = 25;
+const productArray = [];
 
 // ***** DOM WINDOWS ****
 let imgContainer = document.getElementById('img-container');
 let imgOne = document.getElementById('img-one');
 let imgTwo = document.getElementById('img-two');
+let imgThree = document.getElementById('img-three')
 let resultBtn = document.getElementById('show-results-btn');
 let resultsList = document.getElementById('results-container');
 
 // **** CONSTRUCTOR FUNCTION ****
-function Goat(name, imageExtension = 'jpg'){
+function Product(name, imageExtension = 'jpg') {
   this.name = name;
   this.image = `img/${name}.${imageExtension}`;
   this.votes = 0;
@@ -22,82 +23,89 @@ function Goat(name, imageExtension = 'jpg'){
 
 // **** HELPER FUNCTIONS / UTILITIES ****
 
-function randomIndexGenerator(){
-  return Math.floor(Math.random() * goatArray.length);
+function randomIndexGenerator() {
+  return Math.floor(Math.random() * productArray.length);
 }
 
-function renderImgs(){
-  // DONE: get 2 random images on the page
+function renderImgs() {
+  // DONE: get 3 random images on the page
   let imageOneIndex = randomIndexGenerator();
   let imageTwoIndex = randomIndexGenerator();
+  let imageThreeIndex = randomIndexGenerator();
 
   // DONE: make sure they are unique
-  while(imageOneIndex === imageTwoIndex){
+  while (imageOneIndex === imageTwoIndex === imageThreeIndex); {
     imageTwoIndex = randomIndexGenerator();
+    imageOneIndex = randomIndexGenerator();
+    imageThreeIndex = randomIndexGenerator();
   }
 
-  imgOne.src = goatArray[imageOneIndex].image;
-  imgOne.title = goatArray[imageOneIndex].name;
+  imgOne.src = productArray[imageOneIndex].image;
+  imgOne.title = productArray[imageOneIndex].name;
 
-  imgTwo.src = goatArray[imageTwoIndex].image;
-  imgTwo.title = goatArray[imageTwoIndex].name;
+  imgTwo.src = productArray[imageTwoIndex].image;
+  imgTwo.title = productArray[imageTwoIndex].name;
+
+  imgThree.src = productArray[imageThreeIndex].image;
+  imgThree.title = productArray[imageThreeIndex].name;
 
   // DONE: Increase the goats views
-  goatArray[imageOneIndex].views++;
-  goatArray[imageTwoIndex].views++;
+  productArray[imageOneIndex].views++;
+  productArray[imageTwoIndex].views++;
+  productArray[imageThreeIndex].views++;
 }
 
 // **** EVENT HANDLERS ****
-function handleImgClick(event){
-  // DONE: Identify the image that was clicked
+function handleImgClick(event) {
 
   let imageClicked = event.target.title;
   // console.dir(event.target);
   // console.log(imageClicked);
 
-  // TODO: Increase the vote on that image
-  for(let i = 0; i < goatArray.length; i++){
-    if(imageClicked === goatArray[i].name){
-      goatArray[i].votes++;
-      // TODO: decrement the voting round
+  for (let i = 0; i < productArray.length; i++) {
+    if (imageClicked === productArray[i].name) {
+      productArray[i].votes++;
       votingRounds--;
-      // TODO: generate new images
       renderImgs();
     }
   }
 
-  // TODO: once voting are done, we want to remove the ability to click
-  if(votingRounds === 0){
+  if (votingRounds === 0) {
     imgContainer.removeEventListener('click', handleImgClick);
   }
 
 }
 
-function handleShowResults(){
-  if(votingRounds === 0){
-    for(let i = 0; i < goatArray.length; i++){
-      let goatListItem = document.createElement('li');
+function handleShowResults() {
+  if (votingRounds === 0) {
+    for (let i = 0; i < productArray.length; i++) {
+      let productListItem = document.createElement('li');
 
-      goatListItem.textContent = `${goatArray[i].name} - Votes: ${goatArray[i].votes} & Views: ${goatArray[i].views}`;
+      productListItem.textContent = `${productArray[i].name} - Votes: ${productArray[i].votes} & Views: ${productArray[i].views}`;
 
-      resultsList.appendChild(goatListItem);
+      resultsList.appendChild(productListItem);
     }
     resultBtn.removeEventListener('click', handleShowResults);
   }
 }
 
 // **** EXECUTABLE CODE *****
-let bunnyGoat = new Goat('bunny-goat', 'png');
-let coolGoat = new Goat('cool-goat');
-let cruisinGoat = new Goat('cruisin-goat');
-let floatYourGoat = new Goat('float-your-goat');
-let goatOutOfHand = new Goat('goat-out-of-hand');
-let kissingGoat = new Goat('kissing-goat');
-let sassyGoat = new Goat('sassy-goat');
-let smilingGoat = new Goat('smiling-goat');
-let sweaterGoat = new Goat('sweater-goat');
+let chairProd = new Product('chair', 'jpg');
+let cthulhuProd = new Product('cthulu', 'jpg');
+let dogduckProd = new Product('dog-duck', 'jpg');
+let dragonProd = new Product('dragon', 'jpg');
+let penProd = new Product('pen', 'jpg');
+let petsweepProd = new Product('pet-sweep', 'jpg');
+let scissorsProd = new Product('scissors', 'jpg');
+let sharkProd = new Product('shark', 'jpg');
+let sweepProd = new Product('sweep', 'png');
+let tauntaunProd = new Product('tauntaun', 'jpg');
+let unicornProd = new Product('unicorn', 'jpg');
+let watercanProd = new Product('water-can', 'jpg');
+let wineglassProd = new Product('wine-glass', 'jpg');
 
-goatArray.push(bunnyGoat, coolGoat, cruisinGoat, floatYourGoat, goatOutOfHand, kissingGoat, sassyGoat, smilingGoat, sweaterGoat);
+
+productArray.push(chairProd, cthulhuProd, dogduckProd, dragonProd, penProd, petsweepProd, scissorsProd, sharkProd, sweepProd, tauntaunProd, unicornProd, watercanProd, wineglassProd);
 
 
 renderImgs();
