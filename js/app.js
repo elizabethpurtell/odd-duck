1575bd85121e7a9b72ddf466c6266e245438325a
elizabethpurtell <epurtell.CF23@gmail.com>
2023-10-10 11:55:18 -0700

    updated js code for duplicates and unique images

--- js/app.js
+++ js/app.js
@@ -58,6 +58,54 @@ function renderImgs() {
   productArray[imageThreeIndex].views++;
 }
 
+// updated random index generator
+
+let previousRandomIndexArray = []; // Store the previous set of random indices
+
+function getRandomUniqueIndices() {
+  const uniqueIndices = [];
+  
+  while (uniqueIndices.length < 3) {
+    let randomNumber = Math.floor(Math.random() * productArray.length);
+
+    // Check if the random number is not in the previous set
+    if (!uniqueIndices.includes(randomNumber) && !previousRandomIndexArray.includes(randomNumber)) {
+      uniqueIndices.push(randomNumber);
+    }
+  }
+
+  // Update the previous set of random indices
+  previousRandomIndexArray = uniqueIndices.slice();
+
+  return uniqueIndices;
+}
+
+function renderImgs() {
+  const randomIndexArray = getRandomUniqueIndices();
+
+  let imageOneIndex = randomIndexArray[0];
+  let imageTwoIndex = randomIndexArray[1];
+  let imageThreeIndex = randomIndexArray[2];
+
+  imgOne.src = productArray[imageOneIndex].image;
+  imgOne.title = productArray[imageOneIndex].name;
+
+  imgTwo.src = productArray[imageTwoIndex].image;
+  imgTwo.title = productArray[imageTwoIndex].name;
+
+  imgThree.src = productArray[imageThreeIndex].image;
+  imgThree.title = productArray[imageThreeIndex].name;
+
+  // DONE: Increase the product views
+  productArray[imageOneIndex].views++;
+  productArray[imageTwoIndex].views++;
+  productArray[imageThreeIndex].views++;
+}
+
+
+
+
+
 // **** EVENT HANDLERS ****
 function handleImgClick(event) {
 
@@ -118,3 +166,16 @@ renderImgs();
 
 imgContainer.addEventListener('click', handleImgClick);
 resultBtn.addEventListener('click', handleShowResults);
+
+// As a marketeer, I want to prevent users from seeing the same image in two subsequent iterations, so that they are not biased.
+// done: Update your algorithm to randomly generate three unique product images from the images directory.
+// done: Update your algorithm so that new products are generated, confirm that these products are not duplicates from the immediate previous set.
+
+// As a marketing manager, I would like a visual representation of how many times a product was clicked so that I can visually analyze the results.
+
+// TODO: Using ChartJS (imported from CDN), display the vote totals and the number of times a product was viewed in a bar chart format. (hint: don’t forget about the <canvas> tags)
+// TODO: Place the bar chart in the section located beneath your three product images
+// TODO: The bar charts should only appear after all voting data has been collected.
+// TODO: Run a Lighthouse Accessbility report. Make necessary updates to your application based on the report to get your score above 80.
+
+// TODO: Add a screenshot of your score to your README.md file.
